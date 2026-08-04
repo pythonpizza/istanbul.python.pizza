ef4561f17be53a3d6053dae553874248d57d8780
Ege Akman <me@egeakman.dev>
2026-08-04 11:38:01 +0300

    Open the CFP and add dates! (#4)

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,10 +1,10 @@
 export const menuItems = [
   { href: "/#about", label: "About 🍕" },
+  { href: "/#cfp", label: "CFP 🙋🏻‍♀️" },
   { href: "/#sponsors", label: "Sponsors 💛" },
   // { href: "/#schedule", label: "Program 📅" },
   // { href: "/compact-agenda/", label: "Compact agenda 🔗" },
   { href: "/#organizers", label: "Organizers 👩🏻" },
-  { href: "/#cfp", label: "CFP 🙋🏻‍♀️" },
   // { href: "/#venue", label: "Venue 🏰" },
   { href: "/#coc", label: "CoC 💂" },
 ];
